c609b076145d2d0f7b68c2d39400568a4e154eb2
isaacluque <isaacluque3@gmail.com>
2021-11-24 11:55:37 -0600

    mensaje

--- Form3.cs
+++ Form3.cs
@@ -10,17 +10,16 @@ using System.Windows.Forms;
 
 namespace prueba
 {
-    public partial class Form1 : Form
+    public partial class Form3 : Form
     {
-        public Form1()
+        public Form3()
         {
             InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Form siguiente = new Form3();
-            siguiente.Show();
+            MessageBox.Show("Hola");
         }
     }
 }
